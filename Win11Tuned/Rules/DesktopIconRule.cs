5510965bc00836e763275afd4195b6e83fc29608
Kaciras <Kaciras@protonmail.com>
2022-06-30 19:37:36 +0800

    New: desktop icon rules

---
+++ Win11Tuned/Rules/DesktopIconRule.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32;
+
+namespace Win11Tuned.Rules;
+
+public sealed class DesktopIconRule : Rule
+{
+	const string KEY = @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel";
+
+	public string Name {get; private set;}
+
+	public string Description => "在桌面上显示这个图标";
+
+	readonly string clsid;
+
+	public DesktopIconRule(string clsid)
+	{
+		this.clsid = clsid;
+	}
+
+	public bool NeedOptimize()
+	{
+		if (clsid == "{59031a47-3f72-44a7-89c5-5595fe6b30ee}")
+		{
+			Name = "用户的文件";
+		}
+		else
+		{
+			using var key = Registry.ClassesRoot.OpenSubKey(@"CLSID\" + clsid);
+			Name = Utils.ExtractStringResource((string)key.GetValue("LocalizedString"));
+		}
+
+		return (int)Registry.GetValue(KEY, clsid, 1) != 0;
+	}
+
+	public void Optimize() => Registry.SetValue(KEY, clsid, 0);
+}
