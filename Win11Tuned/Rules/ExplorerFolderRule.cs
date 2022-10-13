e60d22cdda01cae393cead4eb923f0f058f85912
Kaciras <Kaciras@protonmail.com>
2022-10-13 20:27:42 +0800

    update namespace clsids of MyComputer

--- Win11Tuned/Rules/ExplorerFolderRule.cs
+++ Win11Tuned/Rules/ExplorerFolderRule.cs
@@ -10,17 +10,16 @@ public sealed class ExplorerFolderRule : Rule
 	const string KEY = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace";
 
 	readonly string[] clsids = {
-		"{1CF1260C-4DD0-4ebb-811F-33C572699FDE}",
-		"{374DE290-123F-4565-9164-39C4925E467B}",
-		"{3ADD1653-EB32-4cb0-BBD7-DFA0ABB5ACCA}",
-		"{A0953C92-50DC-43bf-BE83-3742FED03C9C}",
-		"{A8CDFF1C-4878-43be-B5FD-F8091C1C60D0}",
-		"{B4BFCC3A-DB2C-424C-B029-7FE99A87C641}",
+		"{088e3905-0323-4b02-9826-5d99428e115f}", // Download
+		"{24ad3ad4-a569-4530-98e1-ab02f9417aa8}", // Pictures
+		"{3dfdf296-dbec-4fb4-81d1-6a3438bcf4de}", // Music
+		"{d3162b92-9365-467a-956b-92703aca08af}", // Documents
+		"{f86fa3ab-70d2-4fc7-9c99-fcbf05467f3a}", // Videos
 	};
 
-	public string Name => "删除我的电脑界面上的6个文件夹";
+	public string Name => "删除我的电脑界面上的 5 个文件夹";
 
-	public string Description => "就是视频、文档、链接这些，它们可以从用户目录里打开,放在我的电脑里只会占位置，而且我不咋用";
+	public string Description => "就是视频、文档、下载这些，它们可以从用户目录里打开,放在我的电脑里只会占位置，而且我不咋用";
 
 	public bool NeedOptimize()
 	{
