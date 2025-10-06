3856dc3dba7ec120e315901d10d7c72f5f368db0
Kaciras <contact@kaciras.com>
2025-10-06 15:55:01 +0800

    fix uninstallation of OneDrive

--- Win11Tuned/Rules/SoftwareRuleSet.cs
+++ Win11Tuned/Rules/SoftwareRuleSet.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
-using Microsoft.Win32;
 using Win11Tuned.Properties;
 
 namespace Win11Tuned.Rules;
@@ -40,11 +42,27 @@ public sealed class SoftwareRuleSet : OptimizableSet
 		}
 	}
 
+	// "C:\Users\Kaciras\AppData\Local\Microsoft\OneDrive\25.174.0907.0003\OneDriveSetup.exe"  /uninstall 
 	public void Optimize(string keyName)
 	{
 		using var key = @namespace.OpenSubKey(UNINSTALL + keyName);
-		var cmd = key.GetValue("QuietUninstallString") ?? key.GetValue("UninstallString");
-		var splited = EXE.Split((string)cmd, 2);
-		Process.Start(splited[0] + ".exe", "/quiet " + splited[1]).WaitForExit();
+		var command = (string)(key.GetValue("QuietUninstallString") ?? key.GetValue("UninstallString"));
+
+        // Process.Start 需要分出文件和参数，另一种方法是用 CMD.exe /C
+        var filename = command;
+		var args = "";
+		if (command[0] == '"')
+		{
+			var i = command.IndexOf('"', 1);
+			filename = command.Substring(1, i - 1);
+			args = command.Substring(i + 1);
+		}
+		else
+		{
+			var s = command.Split([' '], 2);
+			filename = s[0];
+			args = s.Length == 2 ? s[1] : "";
+		}
+		Process.Start(filename, "/quiet " + args).WaitForExit();
 	}
 }
